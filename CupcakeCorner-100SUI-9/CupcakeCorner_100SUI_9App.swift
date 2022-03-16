a9b13bb58b38662ca1eeab50b634b82aa245d627
Kuncans <akafauna@gmail.com>
2022-03-16 10:42:51 +0000

    Initial Commit

---
+++ CupcakeCorner-100SUI-9/CupcakeCorner_100SUI_9App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_100SUI_9App.swift
+//  CupcakeCorner-100SUI-9
+//
+//  Created by Duncan Kent on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_100SUI_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
