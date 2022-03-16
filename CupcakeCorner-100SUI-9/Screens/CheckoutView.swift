aeafa797e6b03ed2b588302c578c3139dfd64df3
Kuncans <akafauna@gmail.com>
2022-03-16 12:27:41 +0000

    Add upload encoded order and decode order to alert

--- CupcakeCorner-100SUI-9/Screens/CheckoutView.swift
+++ CupcakeCorner-100SUI-9/Screens/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
     
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -27,14 +30,45 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: { })
-                    .padding()
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
                 
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
     }
+    
+    func placeOrder() async {
+        guard let encodedOrder = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encodedOrder)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes has been confirmed!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
+        
+    }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
